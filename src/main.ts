317f30b46ccf95ee3da131d9e51d2de2d38775be
letengzz <2020885569@qq.com>
2025-03-20 22:55:57 +0800

    feat: add all icons

--- src/main.ts
+++ src/main.ts
@@ -9,10 +9,13 @@ import "@unocss/reset/sanitize/sanitize.css";
 import "@unocss/reset/sanitize/assets.css";
 // 引入全局样式
 // import '@/styles/index.scss'
+import * as ElementPlusIconsVue from "@element-plus/icons-vue";
 
 const app = createApp(App);
 
 useRouter(app);
 usePinia(app);
-
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component);
+}
 app.mount("#app");
